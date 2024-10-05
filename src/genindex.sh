19b8363295601f0106d46eced195bdc2b308a9b9
3ricG <egross73@gmail.com>
2024-10-05 02:03:00 -0400

    Add automatic index page generation, remove existing index.md, remove unused references to sitemap, clean up Makefile

--- src/genindex.sh
+++ src/genindex.sh
@@ -7,10 +7,10 @@ cat << __HEADER__
 title: GROSS.SH
 ---
 
-\
-\
-\
-\
+\\
+\\
+\\
+\\
 
 
 ---
@@ -19,25 +19,42 @@ title: GROSS.SH
 <dl>
 __HEADER__
 
-for f in `find . -type f -iname '*.md' ! -name 'index.md' ! -name 'now.md'`; do
-  first=$(git log --follow --format=%ad --date=format:"%B %d, %Y" $f | head -n 1)
-  last=$(git log --follow --format=%ad --date=format:"%B %d, %Y" $f | tail -n 1)
+# Giant ugly git command to get sorted list of files I care about
+files=$(\
+   git ls-files -z -- '**.md' ':!:index.md' ':!:now.md' ':!:tea.md' | \
+   xargs -0 -I{} -- sh -c 'git log -1 --format="%at {}" {} | tail -1' | \
+   awk '{print $NF}'\
+)
+
+for f in $files; do
+  # Get first and last commit dates, based on git logs
+  last=$(git log --follow --format=%ad --date=format:"%s" $f | head -n 1)
+  first=$(git log --follow --format=%ad --date=format:"%s" $f | tail -n 1)
+  publ=$first
+  
+  # Check for defined first date, and override git if its older
+  orig=$(date --date="$(grep -m 1 -oP "date:\s*\K(.*)" $f)" +"%s")
+  origdiff=($first-$orig)/86400
+  [[  "$origdiff" -gt "0" ]] && publ=$orig
+
+  # Determine if file was ever modified after first commit
+  diff=($last-$first)/86400
   title=$(head -n 5 $f | grep -i title | sed 's/^\s*title:\s*//g')
-  echo "<dt>${first}</dt>"
-  line="<dd>[${title}]($(basename -s .md $f).html) %UPDATED%</dd>"
-  if [[ "$first" == "$last" ]]; then
-    line=$(echo $line | sed "s/%UPDATED%/[Updated: ${last}]/g")
+  publ=$(date +'%b %d, %Y' -d @$publ)
+  last=$(date +'%b %d, %Y' -d @$last)
+  
+  # Add update note if it was modified after first commit
+  line="<dt>${publ}%UPDATED%</dt>"
+  if [[ "$diff" -gt "0" ]]; then
+    line=$(echo $line | sed "s|%UPDATED%|, <span style='font-style:italic;' class='green'>Updated: ${last}</span>|g")
   else
     line=$(echo $line | sed 's/%UPDATED%//g')
   fi
   echo $line
+  echo "<dd>[${title}]($(basename -s .md $f).html) </dd>"
 done
 
 cat << __FOOTER__
 </dl>
 
-
-TODO:
-  - Replace find with git command
-  - Sort files from git original commit date
 __FOOTER__
